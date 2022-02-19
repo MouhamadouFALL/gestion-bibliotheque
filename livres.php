d8a51deec2075d3dd7eefabdb16aeeb174958e80
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-02-19 17:14:47 +0000

    Simplifier l'utilisation de l'objet livreManager

--- livres.php
+++ livres.php
@@ -27,11 +27,14 @@ ob_start();
         <th colspan="2">Actions</th>
     </tr>
 
-    <?php for ($i=0; $i < count($livreManager->getLivres()); $i++) : ?>
+    <?php 
+        $livres = $livreManager->getLivres();
+        for ($i=0; $i < count($livres); $i++) :
+    ?>
     <tr>
-        <td class="align-middle"><img src="public/images/<?= $livreManager->getLivres()[$i]->getImage(); ?>"  width="60px;"></td>
-        <td class="align-middle"><?= $livreManager->getLivres()[$i]->getTitre(); ?></td>
-        <td class="align-middle"><?= $livreManager->getLivres()[$i]->getNbPages(); ?></td>
+        <td class="align-middle"><img src="public/images/<?= $livres[$i]->getImage(); ?>"  width="60px;"></td>
+        <td class="align-middle"><?= $livres[$i]->getTitre(); ?></td>
+        <td class="align-middle"><?= $livres[$i]->getNbPages(); ?></td>
         <td class="align-middle"><a href="" class="btn btn-warning">Modifier</a></td>
         <td class="align-middle"><a href="" class="btn btn-danger">Supprimer</a></td>
     </tr>
