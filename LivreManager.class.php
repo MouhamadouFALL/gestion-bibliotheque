ae3d7f3166c8e76a9af1975fe22aafddf9c6066b
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-02-19 16:31:48 +0000

    Ajout classe Manager des Livres puis son utilisation dans la classe Livre

---
+++ LivreManager.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class LivreManager{
+
+    private $livres;
+
+    public function ajouterLivre($livre) {
+        $this->livres[] = $livre;
+    }
+
+    public function getLivres(){
+        return $this->livres;
+    }
+    
+}
--- livres.php
+++ livres.php
@@ -1,12 +1,19 @@
 <?php 
 
 require_once "Livre.class.php";
-
+require_once "LivreManager.class.php";
 // Creer quatre livres
 $livre1 = new Livre(1, "Algorithme selon H2PROG", "algo.png", 250);
 $livre2 = new Livre(2, "Virus Asiatique", "virus.png", 150);
 $livre3 = new Livre(3, "La France du 19ème", "france.png", 100);
 $livre4 = new Livre(4, "Le JavaScript client", "JS.png", 300);
+
+// l'utiliation de la classe LivreManager
+$livreManager = new LivreManager();
+$livreManager->ajouterLivre($livre1);
+$livreManager->ajouterLivre($livre2);
+$livreManager->ajouterLivre($livre3);
+$livreManager->ajouterLivre($livre4);
 
 ob_start();
 
@@ -20,11 +27,11 @@ ob_start();
         <th colspan="2">Actions</th>
     </tr>
 
-    <?php for ($i=0; $i < count(Livre::$livres); $i++) : ?>
+    <?php for ($i=0; $i < count($livreManager->getLivres()); $i++) : ?>
     <tr>
-        <td class="align-middle"><img src="public/images/<?= Livre::$livres[$i]->getImage(); ?>"  width="60px;"></td>
-        <td class="align-middle"><?= Livre::$livres[$i]->getTitre(); ?></td>
-        <td class="align-middle"><?= Livre::$livres[$i]->getNbPages(); ?></td>
+        <td class="align-middle"><img src="public/images/<?= $livreManager->getLivres[$i]->getImage(); ?>"  width="60px;"></td>
+        <td class="align-middle"><?= $livreManager->getLivres[$i]->getTitre(); ?></td>
+        <td class="align-middle"><?= $livreManager->getLivres[$i]->getNbPages(); ?></td>
         <td class="align-middle"><a href="" class="btn btn-warning">Modifier</a></td>
         <td class="align-middle"><a href="" class="btn btn-danger">Supprimer</a></td>
     </tr>
